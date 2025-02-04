a33b19e52168ee8890b9a0245420a74fd2b0341d
ankit <ankityadavpro0@123>
2025-02-04 12:51:38 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isPublicRoute = createRouteMatcher(['/api/webhooks/clerk'])
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, request) => {
+  // publicRoutes: ["/api/:path*"];
+  if (!isPublicRoute(request)) {
+    auth().protect()
+ }
+ });
 
 export const config = {
   matcher: [
